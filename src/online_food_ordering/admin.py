88c8061bbfadf3b0d69c8d5861103183b45b9576
Kimia Vedad <k.vedad@student.alzahra.ac.ir>
2021-12-27 20:26:36 +0330

    Add other models

--- src/online_food_ordering/admin.py
+++ src/online_food_ordering/admin.py
@@ -1,3 +1,11 @@
+from .models import *
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(Category)
+admin.site.register(Meal)
+admin.site.register(Food)
+admin.site.register(Branch)
+admin.site.register(MenuItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- src/online_food_ordering/models.py
+++ src/online_food_ordering/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
 
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Meal(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Food(models.Model):
+    meal = models.ManyToManyField(Meal)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, null=True, blank=True)
+    image = models.ImageField(upload_to='food_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Branch(models.Model):
+    manager = models.OneToOneField('accounts.RestaurantManager', on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='branches')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='branches')
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'branches'
+
+    def __str__(self):
+        return self.restaurant.name + " " + self.name
+
+
+class MenuItem(models.Model):
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='menu')
+    food = models.ForeignKey(Food, on_delete=models.CASCADE, related_name='menu')
+    stock = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('سفارش', 'سفارش'),
+        ('ثبت', 'ثبت'),
+        ('ارسال', 'ارسال'),
+        ('تحویل', 'تحویل'),
+    ]
+    user = models.ForeignKey('accounts.CustomUser', on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    menu_item = models.ManyToManyField(MenuItem, related_name='orders')
+    quantity = models.PositiveIntegerField()
