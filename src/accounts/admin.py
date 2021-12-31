c4bd7a5572bf26ba8fe4a23731bccf4cc273999c
Kimia Vedad <k.vedad@student.alzahra.ac.ir>
2021-12-31 22:47:17 +0330

    Add save methods to hash password

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,82 +1,106 @@
 from django.contrib import admin
-from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import SiteAdmin, RestaurantManager, Customer, Address
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from .forms import CustomChangeForm, CustomCreationForm
 
 CustomUser = get_user_model()
 
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
+    # model = CustomUser
+    # add_form = CustomCreationForm
+    form = CustomChangeForm
     fieldsets = (
-        ('Personal Info', {'fields': ('username', 'first_name', 'last_name', 'role', 'device', 'address')}),
+        ('Personal Info', {'fields': ('username', 'email', 'first_name', 'last_name', 'role', 'device',)}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
         (None, {'fields': ('email', 'role',)}),
     )
-    list_display = ['email', 'username', 'date_joined']
+    list_display = ['email', 'username', 'date_joined', 'role']
     date_hierarchy = 'date_joined'
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
 
 
 @admin.register(Customer)
 class CustomerProxyAdmin(admin.ModelAdmin):
-    fields = ['email', 'role']
+    fieldsets = (
+        ('Personal Info', {'fields': ('username', 'email', 'first_name', 'last_name', 'device', 'role', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('email',)}),
+    )
     list_display = ['id', 'username', 'role', 'email', ]
     list_display_links = ['id']
     list_editable = ['username']
     search_fields = ['username', 'email']
-    readonly_fields = ['email', ]
+    readonly_fields = ['role', 'is_active', 'is_staff', 'is_superuser']
 
     def get_queryset(self, request):
-        return Customer.objects.filter(is_staff=False)
+        return CustomUser.objects.filter(role=CustomUser.Role.CUSTOMER)
 
-    def has_add_permission(self, request, obj=None):
-        return False
+    def save_model(self, request, obj, form, change):
+        # for hashing password
+        obj.set_password(form.cleaned_data["password"])
+        obj.save()
 
 
 @admin.register(RestaurantManager)
 class RestaurantManagerProxyAdmin(admin.ModelAdmin):
-    fields = ['email', 'role']
-    list_display = ['id', 'first_name', 'role', 'email', ]
-    list_display_links = ['id']
-    list_editable = ['first_name']
-    search_fields = ['username', 'email']
-    readonly_fields = ['email', ]
-    empty_value_display = '-'
-
-    def get_queryset(self, request):
-        return RestaurantManager.objects.filter(is_staff=True, is_superuser=False)
-
-    def has_add_permission(self, request, obj=None):
-        return False
-
-
-@admin.register(SiteAdmin)
-class SiteAdminProxyAdmin(admin.ModelAdmin):
-    fields = ['email', 'role']
+    fieldsets = (
+        ('Personal Info', {'fields': ('username', 'email', 'first_name', 'last_name', 'device', 'role', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('email',)}),
+    )
     list_display = ['id', 'username', 'role', 'email', ]
     list_display_links = ['id']
     list_editable = ['username']
     search_fields = ['username', 'email']
-    readonly_fields = ['email', ]
+    readonly_fields = ['role', 'is_active', 'is_staff', 'is_superuser']
+    empty_value_display = '-'
 
     def get_queryset(self, request):
-        return SiteAdmin.objects.filter(is_superuser=True)
+        return CustomUser.objects.filter(role=CustomUser.Role.RESTAURANT_MANAGER)
 
-    def has_add_permission(self, request, obj=None):
-        return False
+    def save_model(self, request, obj, form, change):
+        # for hashing password
+        obj.set_password(form.cleaned_data["password"])
+        obj.save()
+
+
+@admin.register(SiteAdmin)
+class SiteAdminProxyAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Personal Info', {'fields': ('username', 'email', 'first_name', 'last_name', 'device', 'role', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('email',)}),
+    )
+    list_display = ['id', 'username', 'role', 'email', ]
+    list_display_links = ['id']
+    list_editable = ['username']
+    search_fields = ['username', 'email']
+    readonly_fields = ['role', 'is_active', 'is_staff', 'is_superuser']
+
+    def get_queryset(self, request):
+        return CustomUser.objects.filter(role=CustomUser.Role.SITE_ADMIN)
+
+    def save_model(self, request, obj, form, change):
+        # for hashing password
+        obj.set_password(form.cleaned_data["password"])
+        obj.save()
 
 
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
     list_filter = ['city']
     list_display = ['id', 'city']
-
