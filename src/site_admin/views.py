e28ac4d8e448bb4ec2c36a0d989053c4c772ae57
Kimia Vedad <k.vedad@student.alzahra.ac.ir>
2022-01-16 17:54:59 +0330

    add site admin permissions

--- src/site_admin/views.py
+++ src/site_admin/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView, TemplateView
 from django.urls import reverse_lazy
@@ -5,52 +6,57 @@ from django.urls import reverse_lazy
 from online_food_ordering.models import Food, Category
 
 
-class AdminPanel(TemplateView):
+class AbstractSiteAdmin(LoginRequiredMixin, UserPassesTestMixin):
+
+    def test_func(self):
+        return self.request.user.role == "ادمین سایت"
+
+
+class AdminPanel(AbstractSiteAdmin, TemplateView):
     template_name = 'site_admin/admin_panel.html'
 
 
-class FoodListView(ListView):
+class FoodListView(AbstractSiteAdmin, ListView):
     model = Food
     template_name = 'site_admin/food_list.html'
     fields = '__all__'
 
 
-class FoodCreateView(CreateView):
+class FoodCreateView(AbstractSiteAdmin, CreateView):
     model = Food
     template_name = 'site_admin/food_new.html'
     fields = '__all__'
     success_url = reverse_lazy('food_list')
 
 
-class FoodUpdateView(UpdateView):
+class FoodUpdateView(AbstractSiteAdmin, UpdateView):
     model = Food
     template_name = 'site_admin/food_edit.html'
     fields = '__all__'
     success_url = reverse_lazy('food_list')
 
 
-class FoodDeleteView(DeleteView):
+class FoodDeleteView(AbstractSiteAdmin, DeleteView):
     model = Food
     template_name = 'site_admin/food_delete.html'
     success_url = reverse_lazy('food_list')
 
 
-class CategoryCreateView(CreateView):
+class CategoryCreateView(AbstractSiteAdmin, CreateView):
     model = Category
     template_name = 'site_admin/category_new.html'
     fields = '__all__'
     success_url = reverse_lazy('category_list')
 
 
-class CategoryListView(ListView):
+class CategoryListView(AbstractSiteAdmin, ListView):
     model = Category
     template_name = 'site_admin/category_list.html'
 
 
-class CategoryDetailView(ListView):
+class CategoryDetailView(AbstractSiteAdmin, ListView):
     model = Food
     template_name = 'site_admin/food_list.html'
 
     def get_queryset(self):
         return Food.objects.filter(category=self.kwargs['pk'])
-
